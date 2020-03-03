e0dbc9c8082a842c2c90fcc9af12176baabc4e57
Konrad Grudzinski <2408448g@student.gla.ac.uk>
2020-03-03 12:17:15 +0000

    basic structure, application, media, template, static folders, adjustments to settings.py; second commit

--- cookhub/cookhub_project/settings.py
+++ cookhub/cookhub_project/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "cookhub",
+    "registration",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'cookhub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.media",
             ],
         },
     },
@@ -118,3 +125,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+# Media files (profile & recipe images)
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "/media/"
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_COOKIE_AGE = 3600
+
+# django-registration-redux variables
+
+# If True, users can register
+REGISTRATION_OPEN = True
+
+# If True, the user will be automatically logged in after registration
+REGISTRATION_AUTO_LOGIN = True
+
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = "cookhub_app:index"
+
+# The page users are directed to if they are not logged in.
+LOGIN_URL = "auth_login"
---
+++ cookhub/cookhub_project/urls.py
@@ -0,0 +1,36 @@
+"""cookhub URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from cookhub_app import views
+from django.conf import settings
+from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
+from django.urls import reverse
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, url):
+        return reverse("cookhub_app:register_profile")
+
+urlpatterns = [
+        path('', views.IndexView.as_view(), name='index'),
+        path('cookhub/', include('cookhub_app.urls')),
+        # The above maps any URLs starting with rango/ to be handled by rango.
+        path('admin/', admin.site.urls),
+        path("accounts/register/", MyRegistrationView.as_view(), name="registration_register"),
+        path("accounts/", include("registration.backends.simple.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
