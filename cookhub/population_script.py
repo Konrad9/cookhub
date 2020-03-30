96b0d1521cc58cb901160b9b38795d682457e849
Konrad Grudzinski <2408448g@student.gla.ac.uk>
2020-03-30 14:36:04 +0200

    population script started

---
+++ cookhub/population_script.py
@@ -0,0 +1,11 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+'cookhub_project.settings')
+
+
+def populate():
+    pass
+
+if __name__ == '__main__':
+    print('Starting Cookhub population script...')
+    populate()
