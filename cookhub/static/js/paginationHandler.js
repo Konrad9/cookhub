fa8eef2bf0597911053d6f98a7dd3bb11ae16188
Mihhail Pikkov <2406150p@student.gla.ac.uk>
2020-03-31 18:22:38 +0300

    Small tweaks to thumbnails
    Moved author to a new line, rearranged button

--- cookhub/static/js/paginationHandler.js
+++ cookhub/static/js/paginationHandler.js
@@ -18,7 +18,6 @@ function RecipeGetter(csrf_token, RecipesPerPage, author, which, page, single, e
                          recipeInformation.photoSource = "/media/"+recipe[0];
                          recipeInformation.id = recipe[1];
                          recipeInformation.title = recipe[2];
-                         recipeInformation.author = recipe[3];
                          recipeInformation.averageRating = recipe[3];
                          recipeInformation.creator = recipe[4];
                          if (buttons=="yes") {
@@ -40,9 +39,9 @@ function displayRecipes(element, recipesArray, buttons, which) {
     var part1 = "<div class='col-md-4'><div class='card mb-4 box-shadow'><img class='card-img-top' alt='Thumbnail [100%x225]' style='height: 225px; width: 100%; display: block;' src='";
     var part2 = "' data-holder-rendered='true'><div class='card-body'><a class='card-text-title p-0 m-0 text-truncate' style='max-width: 300px' href='/recipe/";
     var part3 = "'>";
-    var part4 = "</a><div class='d-flex justify-content-between align-items-center'>";
+    var part4 = "</a><div class='d-flex justify-content-between align-items-center m-0 p-0'>";
     var part4Creator = "";
-    var part4Rating = "<p class='card-text-rating mt-2 mb-2'>Rating: ";
+    var part4Rating = "<p class='card-text-rating m-0 t-0'>Rating: ";
     var part5 = "</p>";
     var part6 = "</div></div></div></div>";
     var recipeHTMLpiece = "";
@@ -58,7 +57,7 @@ function displayRecipes(element, recipesArray, buttons, which) {
             buttonPart = "<div class='btn-group'><button type='button' id='" + recipesArray[i].id + "' class='btn btn-sm btn-outline-warning addRecipeButton' data-recipeid='" + recipesArray[i].id + "' disabled='true' >Saved</button></div>";
         }
         if (which!="my") {
-            part4Creator  = "<p class='card-text-author mt-2 mb-2'>Creator:<a href='/profile/" + recipesArray[i].creator + "/'>" + recipesArray[i].creator + "</a></p>";
+            part4Creator  = "<p class='card-text-author mt-0 mb-0'>Creator:<a class='card-text-creator m-0 p-0 pl-2' href='/profile/" + recipesArray[i].creator + "/'>" + recipesArray[i].creator + "</a></p></div><div class='d-flex justify-content-between align-items-center'>";
         }
         recipeHTMLpiece += part1 + recipesArray[i].photoSource + part2 + recipesArray[i].id + part3 + recipesArray[i].title + part4 + part4Creator + part4Rating + recipesArray[i].averageRating + part5 + buttonPart + part6;
     }
